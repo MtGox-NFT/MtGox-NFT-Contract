8759ebc93662ba3f2ece81e534a4c57b50b0aa8e
Mark Karpeles <magicaltux@gmail.com>
2022-02-04 14:46:57 +0900

    add registration date info

--- contracts/MtGoxNFT.sol
+++ contracts/MtGoxNFT.sol
@@ -11,6 +11,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 contract MtGoxNFT is ERC721Enumerable, Ownable {
 	mapping(uint256 => uint256) _fiatWeight;
 	mapping(uint256 => uint256) _satoshiWeight;
+	mapping(uint256 => uint32) _registrationDate;
 	mapping(address => bool) _issuers;
 
 	constructor() ERC721("MtGoxNFT", "MGN") {
@@ -21,9 +22,9 @@ contract MtGoxNFT is ERC721Enumerable, Ownable {
 	}
 
 	// issue will issue a NFT based on a given message
-	function issue(uint256 nftId, address recipient, uint256 fiatWeight, uint256 satoshiWeight, bytes memory signature) public {
+	function issue(uint256 nftId, address recipient, uint256 fiatWeight, uint256 satoshiWeight, uint32 regDate, bytes memory signature) public {
 		// first, check the signature using computeSignature
-		(address recovered, ECDSA.RecoverError error) = ECDSA.tryRecover(computeSignature(nftId, recipient, fiatWeight, satoshiWeight), signature);
+		(address recovered, ECDSA.RecoverError error) = ECDSA.tryRecover(computeSignature(nftId, recipient, fiatWeight, satoshiWeight, regDate), signature);
 		require(error == ECDSA.RecoverError.NoError && _issuers[recovered]);
 
 		// success
@@ -32,10 +33,10 @@ contract MtGoxNFT is ERC721Enumerable, Ownable {
 		_satoshiWeight[1] = satoshiWeight;
 	}
 
-	function computeSignature(uint256 nftId, address recipient, uint256 fiatWeight, uint256 satoshiWeight) public view returns (bytes32) {
+	function computeSignature(uint256 nftId, address recipient, uint256 fiatWeight, uint256 satoshiWeight, uint32 regDate) public view returns (bytes32) {
 		// The signature contains the following elements:
 		// name() "MtGoxNFT", NULL, nftId, recipient(address), fiatWeight, satoshiWeight
-		return ECDSA.toEthSignedMessageHash(abi.encodePacked(name(), uint8(0), nftId, recipient, fiatWeight, satoshiWeight));
+		return ECDSA.toEthSignedMessageHash(abi.encode(name(), uint8(0), nftId, recipient, fiatWeight, satoshiWeight, regDate));
 	}
 
 	function tokenFiatWeight(uint256 tokenId) public view returns (uint256) {
@@ -48,6 +49,12 @@ contract MtGoxNFT is ERC721Enumerable, Ownable {
 		require(_exists(tokenId), "MtGoxNFT: weight query for nonexistent token");
 
 		return _satoshiWeight[tokenId];
+	}
+
+	function tokenRegistrationDate(uint256 tokenId) public view returns (uint256) {
+		require(_exists(tokenId), "MtGoxNFT: registration datequery for nonexistent token");
+
+		return _registrationDate[tokenId];
 	}
 
 	function grantIssuer(address account) public onlyOwner {
