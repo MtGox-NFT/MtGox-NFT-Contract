07d568b93f6df1d669eb903c4383c2267ce9cf25
Mark Karpeles <magicaltux@gmail.com>
2022-03-29 01:39:31 +0900

    upgrade, tests

--- contracts/MtGoxNFT.sol
+++ contracts/MtGoxNFT.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/governance/utils/Votes.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
+pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
 
 // The MtGoxNFT contract is based on the ERC-721 standard with some extra features such as NFT weight
 
-contract MtGoxNFT is ERC721Enumerable, Votes, Ownable {
+contract MtGoxNFT is ERC721, ERC721Enumerable, Ownable, EIP712, ERC721Votes {
 	mapping(address => bool) _issuers;
 
 	// meta-data stored for each NFT
@@ -18,15 +18,11 @@ contract MtGoxNFT is ERC721Enumerable, Votes, Ownable {
 		uint64 satoshiWeight;
 		uint32 registrationDate;
 		uint256 tradeVolume;
+		string url;
 	}
 	mapping(uint256 => MetaInfo) private _meta;
 
-	constructor() ERC721("MtGoxNFT", "MGN") EIP712("MtGoxNFT", "1") {
-	}
-
-	function _baseURI() internal pure override returns (string memory) {
-		return "https://data.mtgoxnft.net/by-id/";
-	}
+	constructor() ERC721("MtGoxNFT", "MGN") EIP712("MtGoxNFT", "1") {}
 
 	function contractURI() public pure returns (string memory) {
 		return "https://data.mtgoxnft.net/contract-meta.json";
@@ -39,6 +35,10 @@ contract MtGoxNFT is ERC721Enumerable, Votes, Ownable {
 		// see: https://docs.opensea.io/docs/metadata-standards
 
 		string memory tokenIdStr = Strings.toString(_tokenId);
+		string memory tokenUrl = _meta[_tokenId].url;
+		if (bytes(tokenUrl).length == 0) {
+			tokenUrl = string(abi.encodePacked("https://data.mtgoxnft.net/by-id/", tokenIdStr, ".png"));
+		}
 
 		// TODO keep this as data uri or just point to a static file? Seems json takes a lot of room in the contract code
 
@@ -50,9 +50,8 @@ contract MtGoxNFT is ERC721Enumerable, Votes, Ownable {
 			"%22,%22external_url%22:%22https://data.mtgoxnft.net/by-id/",
 			tokenIdStr,
 			// image
-			"%22,%22image%22:%22https://data.mtgoxnft.net/by-id/",
-			tokenIdStr,
-			".png",
+			"%22,%22image%22:%22",
+			tokenUrl,
 			// attributes → Registered (date)
 			"%22,%22attributes%22:[%22display_type%22:%22date%22,%22trait_type%22:%22Registered%22,%22value%22:",
 			Strings.toString(_meta[_tokenId].registrationDate),
@@ -68,60 +67,82 @@ contract MtGoxNFT is ERC721Enumerable, Votes, Ownable {
 	}
 
 	// mint NFT as issuer directly (not through sign)
-	function mint(uint256 tokenId, address recipient, uint64 paramFiatWeight, uint64 paramSatoshiWeight, uint32 paramRegDate, uint256 paramTradeVolume) external {
-		require(_issuers[_msgSender()], "MtGoxNFT: method only available to issuers");
-
+	function mint(uint256 tokenId, address recipient, uint64 paramFiatWeight, uint64 paramSatoshiWeight, uint32 paramRegDate, uint256 paramTradeVolume, string memory url) external onlyIssuer {
 		_meta[tokenId] = MetaInfo({
 			fiatWeight: paramFiatWeight,
 			satoshiWeight: paramSatoshiWeight,
 			registrationDate: paramRegDate,
-			tradeVolume: paramTradeVolume
+			tradeVolume: paramTradeVolume,
+			url: url
 		});
-		_mint(recipient, tokenId); // _mint will fail if this NFT was already issued
+		_safeMint(recipient, tokenId); // _mint will fail if this NFT was already issued
 	}
 
-	function fiatWeight(uint256 tokenId) public view returns (uint64) {
+	// set url for a given token
+	function setUrl(uint256 tokenId, string memory url) external onlyIssuer {
+		require(_exists(tokenId), "MtGoxNFT: setUrl for nonexistent NFT");
+		require(bytes(_meta[tokenId].url).length == 0, "MtGoxNFT: cannot set an URL twice");
+
+		_meta[tokenId].url = url;
+	}
+
+	function fiatWeight(uint256 tokenId) external view returns (uint64) {
 		require(_exists(tokenId), "MtGoxNFT: weight query for nonexistent NFT");
 
 		return _meta[tokenId].fiatWeight;
 	}
 
-	function satoshiWeight(uint256 tokenId) public view returns (uint64) {
+	function satoshiWeight(uint256 tokenId) external view returns (uint64) {
 		require(_exists(tokenId), "MtGoxNFT: weight query for nonexistent NFT");
 
 		return _meta[tokenId].satoshiWeight;
 	}
 
-	function registrationDate(uint256 tokenId) public view returns (uint32) {
+	function registrationDate(uint256 tokenId) external view returns (uint32) {
 		require(_exists(tokenId), "MtGoxNFT: registration date query for nonexistent NFT");
 
 		return _meta[tokenId].registrationDate;
 	}
 
-	function tradeVolume(uint256 tokenId) public view returns (uint256) {
+	function tradeVolume(uint256 tokenId) external view returns (uint256) {
 		require(_exists(tokenId), "MtGoxNFT: trade volume query for nonexistent NFT");
 
 		return _meta[tokenId].tradeVolume;
 	}
 
-	function grantIssuer(address account) public onlyOwner {
+	function grantIssuer(address account) external onlyOwner {
 		_issuers[account] = true;
 	}
 
-	function revokeIssuer(address account) public onlyOwner {
+	function revokeIssuer(address account) external onlyOwner {
 		delete _issuers[account];
 	}
 
-	// for votes
-	function _afterTokenTransfer(
-		address from,
-		address to,
-		uint256
-	) internal virtual override {
-		_transferVotingUnits(from, to, 1);
+	modifier onlyIssuer {
+		require(_issuers[_msgSender()], "MtGoxNFT: method only available to issuers");
+		_;
 	}
 
-	function _getVotingUnits(address account) internal virtual override returns (uint256) {
-		return balanceOf(account);
+	// The following functions are overrides required by Solidity.
+
+	function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+		internal
+		override(ERC721, ERC721Enumerable) {
+		super._beforeTokenTransfer(from, to, tokenId);
+	}
+
+	function _afterTokenTransfer(address from, address to, uint256 tokenId)
+		internal
+		override(ERC721, ERC721Votes) {
+		super._afterTokenTransfer(from, to, tokenId);
+	}
+
+	function supportsInterface(bytes4 interfaceId)
+		public
+		view
+		override(ERC721, ERC721Enumerable)
+		returns (bool)
+	{
+		return super.supportsInterface(interfaceId);
 	}
 }
