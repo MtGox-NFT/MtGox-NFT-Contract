3e2491f01aa99a75fddab08ee770aca600bcd024
Mark Karpeles <magicaltux@gmail.com>
2022-04-06 09:39:03 +0900

    encode metadata in base64 because that is what opensea wants

--- contracts/MtGoxNFTmetaLink.sol
+++ contracts/MtGoxNFTmetaLink.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 import "./MtGoxNFT.sol";
 import "./MtGoxNFTmetaLinkInterface.sol";
 
@@ -23,8 +24,10 @@ contract MtGoxNFTmetaLink is Ownable, MtGoxNFTmetaLinkInterface {
 
 		string memory tokenIdStr = Strings.toString(_tokenId);
 		string memory tokenUrl = _contract.getUrl(_tokenId);
+		// TODO uncomment once we have token images
 		if (bytes(tokenUrl).length == 0) {
-			tokenUrl = string(abi.encodePacked("https://data.mtgoxnft.net/by-id/", tokenIdStr, ".png"));
+			//tokenUrl = string(abi.encodePacked("https://data.mtgoxnft.net/by-id/", tokenIdStr, ".png"));
+			tokenUrl = "https://data.mtgoxnft.net/notavailableyet.svg";
 		}
 
 		string memory tokenName;
@@ -33,32 +36,39 @@ contract MtGoxNFTmetaLink is Ownable, MtGoxNFTmetaLinkInterface {
 			tokenName = "Anonymous MtGox NFT";
 		} else {
 			// show the token id (= MtGox account ID)
-			tokenName = string(abi.encodePacked("MtGox NFT %23", tokenIdStr));
+			tokenName = string(abi.encodePacked("MtGox NFT #", tokenIdStr));
 		}
+
+		// Note: OpenSea requires data uri to be base64 encoded
+
+		bytes memory json = abi.encodePacked(
+			// name
+			"{\"name\":\"",
+			tokenName,
+			// external_url
+			"\",\"external_url\":\"https://data.mtgoxnft.net/by-id/",
+			tokenIdStr,
+			// image
+			"\",\"image\":\"",
+			tokenUrl,
+			// attributes → Registered (date)
+			"\",\"attributes\":[{\"display_type\":\"date\",\"trait_type\":\"Registered\",\"value\":",
+			Strings.toString(_contract.registrationDate(_tokenId)),
+			// attributes → Fiat
+			"},{\"trait_type\":\"Fiat\",\"value\":",
+			Strings.toString(_contract.fiatWeight(_tokenId)),
+			"},{\"trait_type\":\"Bitcoin\",\"value\":",
+			Strings.toString(_contract.satoshiWeight(_tokenId)),
+			"},{\"trait_type\":\"Trade Volume\",\"value\":",
+			Strings.toString(_contract.tradeVolume(_tokenId)),
+			"}]}"
+		);
 
 		// TODO keep this as data uri or just point to a static file? Seems json takes a lot of room in the contract code
 
 		return string(abi.encodePacked(
-			// name
-			"data:application/json,{%22name%22:%22",
-			tokenName,
-			// external_url
-			"%22,%22external_url%22:%22https://data.mtgoxnft.net/by-id/",
-			tokenIdStr,
-			// image
-			"%22,%22image%22:%22",
-			tokenUrl,
-			// attributes → Registered (date)
-			"%22,%22attributes%22:[{%22display_type%22:%22date%22,%22trait_type%22:%22Registered%22,%22value%22:",
-			Strings.toString(_contract.registrationDate(_tokenId)),
-			// attributes → Fiat
-			"},{%22trait_type%22:%22Fiat%22,%22value%22:",
-			Strings.toString(_contract.fiatWeight(_tokenId)),
-			"},{%22trait_type%22:%22Bitcoin%22,%22value%22:",
-			Strings.toString(_contract.satoshiWeight(_tokenId)),
-			"},{%22trait_type%22:%22Trade Volume%22,%22value%22:",
-			Strings.toString(_contract.tradeVolume(_tokenId)),
-			"}]}"
+			"data:application/json;base64,",
+			Base64.encode(json)
 		));
 	}
 }
